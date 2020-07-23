73fe8c2170c015107aea8b7dd24111140220e986
KAIYOHUGO <41114603+KAIYOHUGO@users.noreply.github.com>
2020-07-23 22:02:19 +0800

    articles 1

--- js/blog.js
+++ js/blog.js
@@ -3,7 +3,7 @@ var href = document.location.href;
 
 var md = window.markdownit({
   html: true,
-  linkify: true,
+  linkify: false,
   typographer: true,
   highlight: function(str, lang) {
     if (lang && hljs.getLanguage(lang)) {
@@ -21,10 +21,13 @@ $(function() {
   $.get("articles/" + id + ".md", function(data) {
     var result = md.render(data);
     $("#article").html(result);
+    $("title").text($("#article > h1:first-child").text());
+    btp();
   }).fail(function() {
     document.location.pathname = "/404.html";
   });
 });
+
 //coin
 function coin() {
   $("#likecoin").html(
@@ -32,4 +35,23 @@ function coin() {
       href +
       '"></div>'
   );
+}
+//bootstrap
+function btp() {
+  $("img").addClass("img-fluid rounded img-thumbnail mx-auto d-block");
+  $("img").wrap("<picture></picture>");
+  $("img").each(function() {
+    var png = $(this)
+      .attr("src")
+      .replace("png", "webp");
+    var jpg = $(this)
+      .attr("src")
+      .replace("jpg", "webp");
+    if (png > jpg) {
+      var t = png;
+    } else {
+      var t = jpg;
+    }
+    $(this).before(`<source srcset="${t}" type="image/webp" />`);
+  });
 }
