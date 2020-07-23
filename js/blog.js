ab509eb5d604bd0a8792518affd8428a9b3d045f
KAIYOHUGO <41114603+KAIYOHUGO@users.noreply.github.com>
2020-07-23 15:48:12 +0800

    md 3

--- js/blog.js
+++ js/blog.js
@@ -1,8 +1,23 @@
 var id = document.location.hash.replace("#article", "");
 
+var hljs = require("highlight.js");
+// Actual default values
+var md = require("markdown-it")({
+  html: true,
+  typographer: true,
+  highlight: function(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (__) {}
+    }
+    return ""; // use external default escaping
+  }
+});
+
+//load
 $(function() {
   $.get("articles/" + id + ".md", function(data) {
-    var md = window.markdownit();
     var result = md.render(data);
     $("#article").html(result);
   }).fail(function() {
