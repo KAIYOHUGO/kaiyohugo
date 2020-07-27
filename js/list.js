773b66b691eeba0fcff3e113c5e30f39cf6a0be3
KAIYOHUGO <41114603+KAIYOHUGO@users.noreply.github.com>
2020-07-27 18:33:22 +0800

    add list

---
+++ js/list.js
@@ -0,0 +1,6 @@
+$(function() {
+  if (document.location.search != "?q=*") {
+    document.location.search = "?q=*";
+  }
+  $(".gsc-control-cse").addClass("rounded");
+});
--- js/new.js
+++
@@ -1,3 +0,0 @@
-$(function() {
-  $.getJSON("/articles.config.json", function(data) {});
-});
